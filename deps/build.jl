a39519218b1a65ed8bf81e90a4f0f87d6b3970e3
Tony Kelman <tony@kelman.net>
2014-08-05 06:04:02 -0700

    fix Win64 trouble with new binary

--- deps/build.jl
+++ deps/build.jl
@@ -9,17 +9,17 @@ ecos = library_dependency("ecos", aliases=["libecos"])
     provides( Homebrew.HB, "ecos", ecos, os = :Darwin )
 end
 
-# This is the git commit that includes our merged patches as of 07/30/2014
+# This is the git commit that includes our merged patches as of 08/05/2014
 # This is safer than unpacking from master which may cause ECOS.jl to
 # not work properly
-version = "d206a556a83396756f3200964de162b4a7523c62"
+version = "e05a605588c38ad72e843f36af17f8f84e4e2ccb"
 provides(Sources, URI("https://github.com/ifa-ethz/ecos/archive/$version.tar.gz"),
     [ecos], os = :Unix, unpacked_dir="ecos-$version")
 
 prefix = joinpath(BinDeps.depsdir(ecos),"usr")
 srcdir = joinpath(BinDeps.depsdir(ecos),"src","ecos-$version")
 
-provides(Binaries, URI("http://sourceforge.net/projects/juliadeps-win/files/ecos-1.0.3.7z"),
+provides(Binaries, URI("http://sourceforge.net/projects/juliadeps-win/files/ecos-$version.7z"),
     [ecos], unpacked_dir="usr$WORD_SIZE/bin", os = :Windows)
 
 # We'll keep this around for emergencies, but OSX users should be able to use Homebrew

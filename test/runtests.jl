3bbf924e6ed310b8fb708676d97a910f7592175c
Miles Lubin <miles.lubin@gmail.com>
2014-12-28 22:06:37 -0500

    test with new ConicSolverWrapper

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,3 +23,6 @@ import ECOS
 include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
 coniclineartest(ECOS.ECOSSolver(), duals=true)
 conicSOCtest(ECOS.ECOSSolver(), duals=true)
+
+include(joinpath(Pkg.dir("MathProgBase"),"test","quadprog.jl"))
+socptest(MathProgBase.ConicSolverWrapper(ECOS.ECOSSolver()))

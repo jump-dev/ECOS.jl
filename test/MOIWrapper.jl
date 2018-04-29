429f791bf059f146b481e3c2fcf66c634f40bb36
Benoît Legat <benoit.legat@gmail.com>
2018-04-29 11:43:57 +0200

    Add MOI wrapper

---
+++ test/MOIWrapper.jl
@@ -0,0 +1,22 @@
+using MathOptInterfaceECOS
+using Base.Test
+
+using MathOptInterface
+const MOI = MathOptInterface
+const MOIT = MOI.Test
+const MOIB = MOI.Bridges
+
+const MOIU = MOI.Utilities
+MOIU.@model ECOSModelData () (EqualTo, GreaterThan, LessThan) (Zeros, Nonnegatives, Nonpositives, SecondOrderCone, ExponentialCone) () (SingleVariable,) (ScalarAffineFunction,) (VectorOfVariables,) (VectorAffineFunction,)
+const optimizer = MOIU.CachingOptimizer(ECOSModelData{Float64}(), ECOSOptimizer())
+
+# SOC2 requires 1e-5
+const config = MOIT.TestConfig(atol=1e-5, rtol=1e-5)
+
+@testset "Continuous linear problems" begin
+    MOIT.contlineartest(MOIB.SplitInterval{Float64}(optimizer), config)
+end
+
+@testset "Continuous conic problems" begin
+    MOIT.contconictest(optimizer, config, ["rsoc", "geomean", "sdp", "rootdet", "logdet"])
+end

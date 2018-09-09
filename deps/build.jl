14fe38a5d734e4242d1c51cc3734d5cb79a97023
Juan Pablo Vielma <jvielma@mit.edu>
2018-09-09 18:52:10 -0400

    updated binaries installation
    make sure updated versions of binaries are installed

--- deps/build.jl
+++ deps/build.jl
@@ -29,9 +29,11 @@ download_info = Dict(
 
 # Install unsatisfied or updated dependencies:
 unsatisfied = any(!satisfied(p; verbose=verbose) for p in products)
-if unsatisfied && haskey(download_info, platform_key())
+if haskey(download_info, platform_key())
     url, tarball_hash = download_info[platform_key()]
-    if !isinstalled(url, tarball_hash; prefix=prefix) # This check may be redundant
+    # Check if this build.jl is providing new versions of the binaries, and
+    # if so, ovewrite the current binaries even if they were installed by the user 
+    if unsatisfied || !isinstalled(url, tarball_hash; prefix=prefix) 
         # Download and install binaries
         install(url, tarball_hash; prefix=prefix, force=true, verbose=verbose)
     end

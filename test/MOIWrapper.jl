6d562c9e6613663c17984130ade2567120af508f
Benoît Legat <benoit.legat@gmail.com>
2018-10-26 15:35:49 +0200

    ✅ Enable solve_blank_obj fixed in MOI v0.6.2

--- test/MOIWrapper.jl
+++ test/MOIWrapper.jl
@@ -21,11 +21,7 @@ const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
 @testset "Unit" begin
     MOIT.unittest(MOIB.SplitInterval{Float64}(optimizer), config,
-                  [# FIXME The solution of solve_blank_obj is arbitrary,
-                   # ECOS finds 2 instead of the expected 1 but it cannot be
-                   # blamed
-                   "solve_blank_obj",
-                   # Quadratic functions are not supported
+                  [# Quadratic functions are not supported
                    "solve_qcp_edge_cases", "solve_qp_edge_cases",
                    # Integer and ZeroOne sets are not supported
                    "solve_integer_edge_cases", "solve_objbound_edge_cases"])

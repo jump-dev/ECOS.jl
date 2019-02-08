94df0280ec602e9c08ed1ec32de363bb81c8624e
Benoît Legat <benoit.legat@gmail.com>
2019-02-08 01:58:55 +0100

    Simplify thanks to NonposToNoneg bridge

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -1,9 +1,24 @@
+using Compat
+using Compat.Test
+
 using MathOptInterface
 const MOI = MathOptInterface
 const MOIT = MOI.Test
+const MOIU = MOI.Utilities
 const MOIB = MOI.Bridges
 
-const MOIU = MOI.Utilities
+import ECOS
+const optimizer = ECOS.Optimizer(verbose=false)
+
+@testset "SolverName" begin
+    @test MOI.get(optimizer, MOI.SolverName()) == "ECOS"
+end
+
+@testset "supports_allocate_load" begin
+    @test MOIU.supports_allocate_load(optimizer, false)
+    @test !MOIU.supports_allocate_load(optimizer, true)
+end
+
 MOIU.@model(ECOSModelData,
             (), (), # No scalar functions
             (MOI.Zeros, MOI.Nonnegatives, MOI.Nonpositives, MOI.SecondOrderCone,
@@ -12,13 +27,18 @@ MOIU.@model(ECOSModelData,
             (), (), # No scalar sets
             (MOI.VectorOfVariables,),
             (MOI.VectorAffineFunction,))
-const optimizer = MOIU.CachingOptimizer(ECOSModelData{Float64}(), ECOS.Optimizer(verbose=false))
+# UniversalFallback is needed for starting values, even if they are ignored by ECOS
+const cache = MOIU.UniversalFallback(ECOSModelData{Float64}())
+const cached = MOIU.CachingOptimizer(cache, optimizer)
+
+# Essential bridges that are needed for all tests
+const bridged = MOIB.Vectorize{Float64}(MOIB.NonposToNonneg{Float64}(cached))
 
 # SOC2 requires 1e-4
 const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
 @testset "Unit" begin
-    MOIT.unittest(MOIB.SplitInterval{Float64}(MOIB.Vectorize{Float64}(optimizer)),
+    MOIT.unittest(MOIB.SplitInterval{Float64}(bridged),
                   config,
                   [# Quadratic functions are not supported
                    "solve_qcp_edge_cases", "solve_qp_edge_cases",
@@ -27,21 +47,12 @@ const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 end
 
 @testset "Continuous linear problems" begin
-    MOIT.contlineartest(MOIB.SplitInterval{Float64}(MOIB.Vectorize{Float64}(optimizer)),
+    MOIT.contlineartest(MOIB.SplitInterval{Float64}(bridged),
                         config)
 end
 
 @testset "Continuous conic problems" begin
     exclude = ["sdp", "rootdet", "logdet"]
-    MOIT.contconictest(MOIB.GeoMean{Float64}(MOIB.RSOC{Float64}(MOIB.Vectorize{Float64}(optimizer))),
+    MOIT.contconictest(MOIB.GeoMean{Float64}(MOIB.RSOC{Float64}(bridged)),
                        config, exclude)
-end
-
-@testset "SolverName" begin
-    @test MOI.get(optimizer, MOI.SolverName()) == "ECOS"
-end
-
-@testset "supports_allocate_load" begin
-    @test MOIU.supports_allocate_load(optimizer.optimizer, false)
-    @test !MOIU.supports_allocate_load(optimizer.optimizer, true)
 end

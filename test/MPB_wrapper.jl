3235c5713591032a64001dddfbe0030af8d0a83f
odow <o.dowson@gmail.com>
2020-03-31 18:06:22 -0500

    Fix ECOS_jll version

--- test/MPB_wrapper.jl
+++ test/MPB_wrapper.jl
@@ -22,9 +22,7 @@ end
 
 @testset "Run the conic interface test from MathProgBase.jl" begin
     include(joinpath(MPB_test_path, "conicinterface.jl"))
-    # LIN3 unable to be classified as infeasible within iteration limit on
-    # ECOS 2.0.7 and Windows.
-    # coniclineartest(ECOS.ECOSSolver(), duals=true)
+    coniclineartest(ECOS.ECOSSolver(), duals=true)
     conicSOCtest(ECOS.ECOSSolver(), duals=true)
     conicEXPtest(ECOS.ECOSSolver(), duals=true)
     include(joinpath(MPB_test_path, "quadprog.jl"))

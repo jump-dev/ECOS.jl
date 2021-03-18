ed3c3b3016818eb7d7bcf7f0c2e95bd1c71d8e7e
odow <o.dowson@gmail.com>
2021-03-18 14:30:37 +1300

    Add debug print

--- src/gen/libecos_api.jl
+++ src/gen/libecos_api.jl
@@ -56,6 +56,7 @@ end
 
 
 function ECOS_setup(n, m, p, l, ncones, q, nex, Gpr, Gjc, Gir, Apr, Ajc, Air, c, h, b)
+    @show (n, m, p, l, ncones, q, nex, Gpr, Gjc, Gir, Apr, Ajc, Air, c, h, b)
     ccall((:ECOS_setup, ecos), Ptr{pwork}, (idxint, idxint, idxint, idxint, idxint, Ptr{idxint}, idxint, Ptr{pfloat}, Ptr{idxint}, Ptr{idxint}, Ptr{pfloat}, Ptr{idxint}, Ptr{idxint}, Ptr{pfloat}, Ptr{pfloat}, Ptr{pfloat}), n, m, p, l, ncones, q, nex, Gpr, Gjc, Gir, Apr, Ajc, Air, c, h, b)
 end
 

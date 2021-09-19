471ff75ce66f6bbbf03e35ff2ae61ffc3c07d88d
Benoît Legat <benoit.legat@gmail.com>
2021-09-19 06:43:58 +0200

    Update from DeprecatedTest Test

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -1,72 +1,56 @@
+module TestECOS
+
 using Test
-
 using MathOptInterface
-const MOI = MathOptInterface
-const MOIDT = MOI.DeprecatedTest
-const MOIU = MOI.Utilities
-const MOIB = MOI.Bridges
-
 import ECOS
-const optimizer = ECOS.Optimizer()
-MOI.set(optimizer, MOI.Silent(), true)
 
-@testset "SolverName" begin
-    @test MOI.get(optimizer, MOI.SolverName()) == "ECOS"
+const MOI = MathOptInterface
+
+function runtests()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$(name)", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    return
 end
 
-# UniversalFallback is needed for starting values, even if they are ignored by ECOS
-const cache = MOIU.UniversalFallback(MOIU.Model{Float64}())
-const cached = MOIU.CachingOptimizer(cache, optimizer)
-
-const bridged = MOIB.full_bridge_optimizer(cached, Float64)
-
-# SOC2 requires 1e-4
-const CONFIG = MOIDT.Config(atol = 1e-4, rtol = 1e-4)
-
-@testset "Unit" begin
-    MOIDT.unittest(
-        bridged,
-        CONFIG,
-        [
-            # `NumberOfThreads` not supported.
-            "number_threads",
-            # `TimeLimitSec` not supported.
-            "time_limit_sec",
-            # Need https://github.com/jump-dev/MathOptInterface.jl/issues/529
-            "solve_qp_edge_cases",
-            # Integer and ZeroOne sets are not supported
-            "solve_integer_edge_cases",
-            "solve_objbound_edge_cases",
-            "solve_zero_one_with_bounds_1",
-            "solve_zero_one_with_bounds_2",
-            "solve_zero_one_with_bounds_3",
+function test_runtests()
+    solver = MOI.OptimizerWithAttributes(ECOS.Optimizer, MOI.Silent() => true)
+    model = MOI.instantiate(solver, with_bridge_type=Float64)
+    MOI.Test.runtests(
+        model,
+        MOI.Test.Config(
+            atol = 1e-3,
+            rtol = 1e-3,
+            exclude = Any[
+                MOI.ConstraintBasisStatus,
+                MOI.VariableBasisStatus,
+                MOI.ConstraintName,
+                MOI.VariableName,
+                MOI.ObjectiveBound,
+            ],
+        ),
+        exclude = String[
+            # Expected test failures:
+            #   Problem is a nonconvex QP
+            "test_basic_ScalarQuadraticFunction_EqualTo",
+            "test_basic_ScalarQuadraticFunction_GreaterThan",
+            "test_basic_ScalarQuadraticFunction_Interval",
+            "test_basic_VectorQuadraticFunction_",
+            "test_quadratic_SecondOrderCone_basic",
+            "test_quadratic_nonconvex_",
+            #   MathOptInterface.jl issue #1431
+            "test_model_LowerBoundAlreadySet",
+            "test_model_UpperBoundAlreadySet",
         ],
     )
+    return
 end
 
-@testset "Continuous linear problems" begin
-    MOIDT.contlineartest(bridged, CONFIG)
-end
-
-@testset "Continuous quadratic problems" begin
-    MOIDT.qcptest(bridged, CONFIG)
-end
-
-@testset "Continuous conic problems" begin
-    exclude = [
-        "dualexp",
-        "pow",
-        "dualpow",
-        "sdp",
-        "rootdet",
-        "logdet",
-        "normnuc",
-        "normspec",
-    ]
-    MOIDT.contconictest(bridged, CONFIG, exclude)
-end
-
-@testset "MOI.RawOptimizerAttribute" begin
+function test_RawOptimizerAttribute()
     model = ECOS.Optimizer()
     MOI.set(model, MOI.RawOptimizerAttribute("abstol"), 1e-5)
     @test MOI.get(model, MOI.RawOptimizerAttribute("abstol")) ≈ 1e-5
@@ -74,21 +58,24 @@ end
     @test MOI.get(model, MOI.RawOptimizerAttribute("abstol")) == 2e-5
 end
 
-@testset "Iteration Limit" begin
+
+function test_iteration_limit()
     # Problem data
     v = [5.0, 3.0, 1.0]
     w = [2.0, 1.5, 0.3]
 
-    MOI.empty!(bridged)
+
+    solver = MOI.OptimizerWithAttributes(ECOS.Optimizer, MOI.Silent() => true)
+    model = MOI.instantiate(solver, with_bridge_type=Float64)
 
     maxit = 1
-    MOI.set(bridged, MOI.RawOptimizerAttribute("maxit"), maxit)
-    MOI.set(bridged, MOI.Silent(), true)
+    MOI.set(model, MOI.RawOptimizerAttribute("maxit"), maxit)
+    MOI.set(model, MOI.Silent(), true)
 
-    x = MOI.add_variables(bridged, 3)
+    x = MOI.add_variables(model, 3)
     for xj in x
         MOI.add_constraint(
-            bridged,
+            model,
             MOI.SingleVariable(xj),
             MOI.Interval(0.0, 1.0),
         )
@@ -96,21 +83,25 @@ end
 
     # Constraint
     MOI.add_constraint(
-        bridged,
+        model,
         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(w, x), 0.0),
         MOI.LessThan(3.0),
     )
 
     # Set the objective
     MOI.set(
-        bridged,
+        model,
         MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(v, x), 0.0),
     )
-    MOI.set(bridged, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
 
-    MOI.optimize!(bridged)
+    MOI.optimize!(model)
 
-    @test MOI.get(bridged, MOI.TerminationStatus()) == MOI.ITERATION_LIMIT
-    @test MOI.get(bridged, MOI.BarrierIterations()) == maxit
+    @test MOI.get(model, MOI.TerminationStatus()) == MOI.ITERATION_LIMIT
+    @test MOI.get(model, MOI.BarrierIterations()) == maxit
 end
+
+end  # module
+
+TestECOS.runtests()

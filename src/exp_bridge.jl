1716cec09b48e8cfab1fd5d79d8c8f2a168e71b5
odow <o.dowson@gmail.com>
2021-11-29 10:05:04 +1300

    Various formatting improvements

--- src/exp_bridge.jl
+++ src/exp_bridge.jl
@@ -1,11 +1,11 @@
-# ECOS orders differently than MOI the second and third dimension of the exponential cone
+# ECOS orders differently than MOI the second and third dimension of the
+# exponential cone
 
 struct PermutedExponentialCone <: MOI.AbstractVectorSet end
+
 Base.copy(set::PermutedExponentialCone) = set
 
-const MOIB = MOI.Bridges
-
-struct PermutedExponentialBridge{T,F} <: MOIB.Constraint.SetMapBridge{
+struct PermutedExponentialBridge{T,F} <: MOI.Bridges.Constraint.SetMapBridge{
     T,
     PermutedExponentialCone,
     MOI.ExponentialCone,
@@ -15,7 +15,7 @@ struct PermutedExponentialBridge{T,F} <: MOIB.Constraint.SetMapBridge{
     constraint::MOI.ConstraintIndex{F,PermutedExponentialCone}
 end
 
-function MOIB.Constraint.concrete_bridge_type(
+function MOI.Bridges.Constraint.concrete_bridge_type(
     ::Type{<:PermutedExponentialBridge{T}},
     F::Type{<:MOI.AbstractVectorFunction},
     ::Type{MOI.ExponentialCone},
@@ -23,43 +23,49 @@ function MOIB.Constraint.concrete_bridge_type(
     return PermutedExponentialBridge{T,F}
 end
 
-function MOIB.map_set(
+function MOI.Bridges.map_set(
     ::Type{<:PermutedExponentialBridge},
     ::MOI.ExponentialCone,
 )
     return PermutedExponentialCone()
 end
 
-function MOIB.inverse_map_set(
+function MOI.Bridges.inverse_map_set(
     ::Type{<:PermutedExponentialBridge},
     ::PermutedExponentialCone,
 )
     return MOI.ExponentialCone()
 end
 
-function MOIB.map_function(
+function MOI.Bridges.map_function(
     ::Type{<:PermutedExponentialBridge{T}},
     func,
 ) where {T}
-    scalars = MOIU.eachscalar(func)
+    scalars = MOI.Utilities.eachscalar(func)
     return scalars[[1, 3, 2]]
 end
 
 # Swapping indices 2 <-> 3 is an involution (it is its own inverse)
 
-function MOIB.inverse_map_function(BT::Type{<:PermutedExponentialBridge}, func)
-    return MOIB.map_function(BT, func)
+function MOI.Bridges.inverse_map_function(
+    BT::Type{<:PermutedExponentialBridge},
+    func,
+)
+    return MOI.Bridges.map_function(BT, func)
 end
 
 # It is also symmetric (it is its own transpose)
 
-function MOIB.adjoint_map_function(BT::Type{<:PermutedExponentialBridge}, func)
-    return MOIB.map_function(BT, func)
-end
-
-function MOIB.inverse_adjoint_map_function(
+function MOI.Bridges.adjoint_map_function(
     BT::Type{<:PermutedExponentialBridge},
     func,
 )
-    return MOIB.map_function(BT, func)
+    return MOI.Bridges.map_function(BT, func)
+end
+
+function MOI.Bridges.inverse_adjoint_map_function(
+    BT::Type{<:PermutedExponentialBridge},
+    func,
+)
+    return MOI.Bridges.map_function(BT, func)
 end

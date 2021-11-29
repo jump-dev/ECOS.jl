d9a184b77f2e6fbe7d9cf7e21b91ea64bda1c318
odow <o.dowson@gmail.com>
2021-11-29 17:15:06 +1300

    Fix

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -18,8 +18,27 @@ function runtests()
 end
 
 function test_runtests()
-    solver = MOI.OptimizerWithAttributes(ECOS.Optimizer, MOI.Silent() => true)
-    model = MOI.instantiate(solver, with_bridge_type = Float64)
+    model = MOI.instantiate(ECOS.Optimizer, with_bridge_type = Float64)
+    MOI.set(model, MOI.Silent(), true)
+    exclude = String[
+        # Expected test failures:
+        #   Problem is a nonconvex QP (fixed in MOI 0.10.6)
+        "test_basic_ScalarQuadraticFunction_EqualTo",
+        "test_basic_ScalarQuadraticFunction_GreaterThan",
+        "test_basic_ScalarQuadraticFunction_Interval",
+        "test_basic_VectorQuadraticFunction_",
+        "test_quadratic_SecondOrderCone_basic",
+        "test_quadratic_nonconvex_",
+        #   MathOptInterface.jl issue #1431
+        "test_model_LowerBoundAlreadySet",
+        "test_model_UpperBoundAlreadySet",
+    ]
+    if Sys.WORD_SIZE == 32
+        # These tests fail on x86 Linux, returning ITERATION_LIMIT instead of
+        # proving {primal,dual}_INFEASIBLE.
+        push!(exclude, "test_conic_linear_INFEASIBLE")
+        push!(exclude, "test_solve_TerminationStatus_DUAL_INFEASIBLE")
+    end
     MOI.Test.runtests(
         model,
         MOI.Test.Config(
@@ -28,24 +47,10 @@ function test_runtests()
             exclude = Any[
                 MOI.ConstraintBasisStatus,
                 MOI.VariableBasisStatus,
-                MOI.ConstraintName,
-                MOI.VariableName,
                 MOI.ObjectiveBound,
             ],
         ),
-        exclude = String[
-            # Expected test failures:
-            #   Problem is a nonconvex QP
-            "test_basic_ScalarQuadraticFunction_EqualTo",
-            "test_basic_ScalarQuadraticFunction_GreaterThan",
-            "test_basic_ScalarQuadraticFunction_Interval",
-            "test_basic_VectorQuadraticFunction_",
-            "test_quadratic_SecondOrderCone_basic",
-            "test_quadratic_nonconvex_",
-            #   MathOptInterface.jl issue #1431
-            "test_model_LowerBoundAlreadySet",
-            "test_model_UpperBoundAlreadySet",
-        ],
+        exclude = exclude,
     )
     return
 end
